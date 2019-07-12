dd8f32be4eba270e04833ba36b2d63bca6919356
Wirtane <52761933+Wirtane@users.noreply.github.com>
2019-07-12 18:40:44 +0300

    Logiikka v0.1
    Lisätty selityksen lisäys englanniksi ja selkeytetty parin metodin nimentää. Tyhjän metodin arvoSelityksenKieli toteutus menee seuraavaan kertaan.

--- Logiikka.java
+++ Logiikka.java
@@ -1,13 +1,16 @@
 package kertauspeli;
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class Logiikka {
     
     private HashMap<String, String> parit;
+    private HashMap<String, String> pairs;
     
     public Logiikka(){
         this.parit = new HashMap<>();
+        this.pairs = new HashMap<>();
     }
     
     public void lisaa(String avain, String arvo){
@@ -16,11 +19,30 @@ public class Logiikka {
         }
     }
     
-    public String annaSelitys(String sana){
+    public void lisaaEnglanniksi(String arvo, String avain){
+        if(!(this.pairs.containsKey(avain))){
+            this.pairs.put(arvo, avain);
+        }
+    }
+    
+    public String selitysSuomeksi(String sana){
         if(!this.parit.keySet().contains(sana)){
             return "Sanalle ei ole selitystä!";
         }
         
          return this.parit.get(sana);
     }
+    
+    public String selitysEnglanniksi(String sana){
+        if(!this.pairs.keySet().contains(sana)){
+            return "Sanalle ei ole selitystä!";
+        }
+        
+         return this.pairs.get(sana);
+    }
+    
+    public void arvoSelityksenKieli(){
+        
+    }
 }
+
