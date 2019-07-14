be095da4792c3ca312a7055a49cdc453545bc66c
Wirtane <52761933+Wirtane@users.noreply.github.com>
2019-07-14 22:43:09 +0300

    Logiikka v0.3
    Lisäsin metodille arvoSelityksenKieli toiminnallisuuden, korjasin metodin lisaaEnglanniksi termejä ja aloitin metodin haeTermi tekemisen. Tein kyseistä metodia varten ArrayListin, josta voisi hakea syötettyjä avaimia, mutta sen lopullinen toteutus jää ensi kertaan.

--- Logiikka.java
+++ Logiikka.java
@@ -2,26 +2,37 @@ package kertauspeli;
 
 import java.util.HashMap;
 import java.util.Random;
+import java.util.ArrayList;
 
 public class Logiikka {
     
     private HashMap<String, String> parit;
     private HashMap<String, String> pairs;
+    private ArrayList<String> avaimet;
     
     public Logiikka(){
         this.parit = new HashMap<>();
         this.pairs = new HashMap<>();
+        this.avaimet = new ArrayList<>();
     }
     
     public void lisaa(String avain, String arvo){
         if(!this.parit.containsKey(avain)){
             this.parit.put(avain, arvo);
         }
+        
+        if(!(this.avaimet.contains(avain))){
+            this.avaimet.add(avain);
+        }
     }
     
-    public void lisaaEnglanniksi(String arvo, String avain){
+    public void lisaaEnglanniksi(String avain, String arvo){
         if(!(this.pairs.containsKey(avain))){
-            this.pairs.put(arvo, avain);
+            this.pairs.put(avain, arvo);
+        }
+        
+        if(!(this.avaimet.contains(avain))){
+            this.avaimet.add(avain);
         }
     }
     
@@ -41,8 +52,25 @@ public class Logiikka {
          return this.pairs.get(sana);
     }
     
-    public void arvoSelityksenKieli(){
+    public String arvoSelityksenKieli(String sana){
+        Random rnd = new Random();
+        int luku = rnd.nextInt(2);
         
+        if(luku == 0){
+           return this.selitysEnglanniksi(sana);
+        }else{
+            return this.selitysSuomeksi(sana);
+        }
+    }
+    
+    public String haeTermi(){
+        Random rnd = new Random();
+        int luku = rnd.nextInt(this.parit.size());
+        
+        for(String termi: this.parit.keySet()){
+            
+        }
+        return "";
     }
 }
 
